00440c9e08343a5e84afd524cb115f74f357d0ed
duowngng <duowng.ng@gmail.com>
2023-08-23 15:33:46 +0700

    Add table to admin page

--- base/admin.py
+++ base/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 
-from .models import User
+from .models import User, Topic, Subtopic, Matching, Question, Answer
 
 admin.site.register(User)
+admin.site.register(Topic)
+admin.site.register(Subtopic)
+admin.site.register(Matching)
+admin.site.register(Question)
+admin.site.register(Answer)
