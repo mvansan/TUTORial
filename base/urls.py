ee1dffeb48eef8820ea56389f8d60ee80d9346b7
duowngng <duowng.ng@gmail.com>
2023-08-24 16:04:51 +0700

    Update

--- base/urls.py
+++ base/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name="home"),
+    path('question/', views.questionList, name="questionList"),
+    path('question/<str:pk>/', views.question, name="question"),
+    path('create-question/', views.createQuestion, name="create-question")
 ]
