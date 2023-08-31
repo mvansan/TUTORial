26f7549616adc234a1e0a7afc0c9924593d740e8
duowngng <duowng.ng@gmail.com>
2023-08-31 08:30:35 +0700

    Update matching

--- base/admin.py
+++ base/admin.py
@@ -2,11 +2,13 @@ from django.contrib import admin
 
 from .models import User, Topic, Matching, Question, Answer, Review, UserInfo
 
+class MatchingAdmin(admin.ModelAdmin):
+    list_display = ("user", "topic", "salary","point","time",)
+
 admin.site.register(User)
 admin.site.register(Topic)
-# admin.site.register(Matching, MatchingAdmin)
+admin.site.register(Matching, MatchingAdmin)
 admin.site.register(Question)
 admin.site.register(Answer)
-# admin.site.register(Point)
 admin.site.register(UserInfo)
 admin.site.register(Review)
