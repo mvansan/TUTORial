d5a13b45be26085b6e396316862accdd7f0a5634
duowngng <duowng.ng@gmail.com>
2023-08-31 18:01:30 +0700

    Fix migrations

--- base/migrations/0020_contact_matching_matching_count.py
+++ base/migrations/0020_contact_matching_matching_count.py
@@ -10,19 +10,7 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.CreateModel(
-            name='Contact',
-            fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('title', models.CharField(max_length=100)),
-                ('content', models.TextField()),
-                ('date', models.DateField(auto_now_add=True)),
-                ('email', models.EmailField(max_length=200)),
-            ],
-            options={
-                'db_table': 'contacts',
-            },
-        ),
+        
         migrations.AddField(
             model_name='matching',
             name='matching_count',
